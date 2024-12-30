c0dbafbebd470ec7ccd0035e3f82929c5a741d13
Timothy Lim <timlimytn@gmail.com>
2024-12-30 15:25:22 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://ec2-beanstalk-test.ap-southeast-1.elasticbeanstalk.com",
+  uri: "https://ec2.devdomain.cloud/graphql",
   cache: new InMemoryCache(),
 });
 
